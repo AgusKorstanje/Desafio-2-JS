1894a574bff8557d5751f2c6eeda9d3ff29d307d
AgusKorstanje <agus.korstanje@gmail.com>
2022-09-14 19:28:57 -0300

    version-2

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
+let examen = 1;
 let nota1;
 let nota2;
 let notaf; /*nota promedio*/
 let notarec;  /*nota recuperatorio*/
 let final;  
 let mensajef;   /*mensaje final*/
+function limites(valor){
+    while( valor < 0 || valor > 10 || valor == isNaN){
+        alert("nota no valida")
+        if(examen === 1){
+            valor = parseInt(prompt("PARCIAL 1"))
+            nota1 = valor;
+        }
+        else if(examen === 2){
+            valor = parseInt(prompt("PARCIAL 2"))
+            nota2 = valor;
+        }
+        else if(examen === 3){
+            valor = parseInt(prompt("nota del final"))
+            final = valor;
+        }
+    }
+}
 
 alert("Bienvenido al calculador de notas, se va a solicitar el nombre del alumno y notas de sus examenes, en caso de queres abandonar tipee 'ESC' cuando se soliite alumno")
 let alumno = prompt("ingrese nombre del alumno");
@@ -11,15 +29,12 @@ alumno = alumno.toUpperCase("");
 
 while(alumno != "ESC"){
     nota1 = parseInt(prompt("PARCIAL 1"));
-    while(nota1 < 0 || nota1 > 10 || isNaN(nota1)){
-        alert("nota no valida");
-        nota1 = parseInt(prompt("PARCIAL 1"));
-    }
+    limites(nota1);
+    alert(`${nota1}`);
+    examen = examen + 1;
     nota2 = parseInt(prompt("PARCIAL 2"));
-    while(nota2 < 0 || nota2 > 10 || isNaN(nota2)){
-        alert("nota no valida");
-        nota2 = parseInt(prompt("PARCIAL 2"));
-    }
+    limites(nota2);
+    examen = examen + 1;
     notaf = nota1 + nota2;
     /* la nota llega para final?? */
     if(notaf < 8 || nota1 == 1 || nota2 == 1){
@@ -43,10 +58,7 @@ while(alumno != "ESC"){
     else if(notaf < 13) {
         alert("vas a final");
         final = parseInt(prompt("nota del final"));
-        while(final < 0 || final > 10 || isNaN(final)){
-            alert("nota no valida");
-            final = parseInt(prompt("nota del final"));
-        }
+        limites(final)
         notaf = final;
     }
     else {
@@ -54,6 +66,7 @@ while(alumno != "ESC"){
         notaf = Math.round((nota1 + nota2)/2);
     }
     mensajef = alert(`la nota final de ${alumno} es ${notaf}`);
+    examen = 1;
     alumno = prompt("ingrese nombre del alumno");
     alumno = alumno.toUpperCase("");
 }
